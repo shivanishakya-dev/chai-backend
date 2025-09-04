4aa91b1f92fa0474fbce68915c2487181ff20fe6
Your Name <your-email@example.com>
2025-09-04 09:51:21 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,52 @@
+// require('dotenv').config({path : './env'})
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express()(async () => {
+
+  try {
+    mongoose.connect(`${process.env.MONGOSDB_URL}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("ERROR: ", error);
+      throw error
+    });
+
+    app.listen(process.env.PORT, () => {
+        console.log(`app is listening on port ${process.env.PORT}`);
+    })
+
+  } catch (error) {
+    console.error("ERROR:", error);
+    throw err;
+  }
+})();
+*/
