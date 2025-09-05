20db451eb850db6294e4c663514af5115c191688
Your Name <your-email@example.com>
2025-09-05 16:25:28 +0530

    utils code discusstion

--- src/index.js
+++ src/index.js
@@ -7,9 +7,18 @@ dotenv.config({
 })
 
 
-
-
 connectDB()
+.then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is runing at port : 
+      ${process.env.PORT}`);
+    
+  })
+})
+.catch((err) => {
+  console.log("MONGO db connection failed !!!", err);
+  
+})
 
 
 
